4665ebd4de2e3e74dad62e1315ee1b8760537d0d
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-06-05 01:39:30 +0530

    Updted Max Lengtht pair file

--- Greedy/MaxLengthtPair.java
+++ Greedy/MaxLengthtPair.java
@@ -1,22 +1,48 @@
+//package Greedy;
+//import java.util.*;
+//public class MaxLengthtPair {
+//	public static void main(String args[])
+//	{
+//		int pairs[][]= {{5,24},{39,60},{5,20},{27,40},{50,90}};
+//		Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+//		
+//		int chainLen=1; //for counting
+//		int chainEnd=pairs[0][1];
+//		
+//		for(int i=1;i<pairs.length;i++) 
+//		{
+//			if(pairs[i][0] > chainEnd)
+//			{
+//				chainLen++;
+//				chainEnd=pairs[i][1];
+//			}
+//		}
+//		System.out.println("Maximum length of chain "+chainLen);
+//	}
+//}
+
+
 package Greedy;
 import java.util.*;
-public class MaxLengthtPair {
+public class MaxLengthtPair
+{
 	public static void main(String args[])
 	{
 		int pairs[][]= {{5,24},{39,60},{5,20},{27,40},{50,90}};
+		
 		Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
 		
-		int chainLen=1; //for counting
+		int chainCount=1;
 		int chainEnd=pairs[0][1];
 		
-		for(int i=1;i<pairs.length;i++) 
+		for(int i=1;i<pairs.length;i++)
 		{
-			if(pairs[i][0] > chainEnd)
+			if(chainEnd < pairs[i][0])
 			{
-				chainLen++;
+				chainCount++;
 				chainEnd=pairs[i][1];
 			}
 		}
-		System.out.println("Maximum length of chain "+chainLen);
+		System.out.println("Longest length of chain "+chainCount);
 	}
 }
