0eaa2c4c5437e8ac05f8d1500304b0453c79c38c
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-06-06 10:09:47 +0530

    Updated Indian Coins file

--- Greedy/IndianCoins.java
+++ Greedy/IndianCoins.java
@@ -1,5 +1,55 @@
+//package Greedy;
+//import java.util.*;
+//public class IndianCoins {
+//	public static void main(String args[])
+//	{
+//		Integer coins[]= {1,2,5,10,20,50,100,500,2000};
+//		Arrays.sort(coins, Comparator.reverseOrder());
+//		int countCoins=0;
+//		int amount=590;
+//		
+//		ArrayList<Integer> ans=new ArrayList<>();
+//		
+//		for(int i=0;i<coins.length;i++)
+//		{
+//			if(coins[i]<=amount)
+//			{
+//				while(coins[i]<=amount)
+//				{
+//					countCoins++;
+//					ans.add(coins[i]);
+//					amount-=coins[i];
+//
+//				}
+//			}
+//		}
+//		System.out.println("Total coins used "+countCoins);
+//	}
+//}
+
+
 package Greedy;
-
-public class IndianCoins {
-
+import java.util.*;
+public class IndianCoins
+{
+	public static void main(String args[])
+	{
+		Integer coins[]= {1,2,5,10,20,50,100,500,2000}; //arrays.sort works only on objects
+		Arrays.sort(coins, Comparator.reverseOrder());
+		
+		int countCoins=0;
+		int amount=590;
+		
+		for(int i=0;i<coins.length;i++)
+		{
+			while(coins[i]<=amount)
+			{
+				countCoins++;
+				System.out.print(coins[i]+" ");
+				amount-=coins[i];
+			}
+		}
+		System.out.println();
+		System.out.println("Total number of coins used "+countCoins);
+	}
 }
