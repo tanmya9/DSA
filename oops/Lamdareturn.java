1720dcd357707842a269356709656c460c7ab017
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-09-18 23:28:22 -0700

    Add Lambda with return

---
+++ oops/Lamdareturn.java
@@ -0,0 +1,18 @@
+@FunctionalInterface 
+interface Demo
+{
+    public int show(int i, int j);
+}
+public class Lamdareturn {
+    public static void main(String args[])
+    {
+        Demo obj=new Demo(){
+            public int show(int i, int j)
+            {
+                return i+j;
+            }
+        };
+        int result= obj.show(5,6);
+        System.out.println("Sum is: "+result);
+    }
+}
