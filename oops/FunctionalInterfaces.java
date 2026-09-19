e91cba4e65ac134f02ef4ad0c9f01a2a42ff367a
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-09-18 23:22:52 -0700

    Add Lambda

--- oops/FunctionalInterfaces.java
+++ oops/FunctionalInterfaces.java
@@ -1,6 +1,7 @@
 @FunctionalInterface
 interface A{
-    void show(); //in functional interface we can have only one method
+    //void show(); //in functional interface we can have only one method
+    void show (int i);
 }
 
 // class B implements A{  //class not needed when using anonymous in a class
@@ -10,7 +11,7 @@ interface A{
 //     }
 // }
 
-public class FuntionalInterfaces {
+public class FunctionalInterfaces {
     //A obj = new A (); //we cannot instantiate interfaces
     public static void main(String args[])
     {   
@@ -20,13 +21,33 @@ public class FuntionalInterfaces {
 
         //2nd method - without creating B class and creating an object of B
         // we would be using anonymous in a class
-        A obj = new A(){ //instantiating A by defining its own implementation
-            public void show()
-            {
-                System.out.println("In show method");
-            }
+        // A obj = new A(){ //instantiating A by defining its own implementation
+        //     public void show()
+        //     {
+        //         System.out.println("In show method");
+        //     }
+        // };
+
+        // A obj=new A(){
+        //     public void show(int i)
+        //     {
+        //         System.out.println("In show method "+i);
+        //     }
+        // };
+        // obj.show(5);
+
+        //lambda expression
+        // A obj = () ->
+        // {
+        //     System.out.println("in show method");
+        // };
+        // obj.show();
+
+        //lambda expression
+        A obj=(int i) -> {
+            System.out.println("In show method "+i);
         };
-        obj.show();
+        obj.show(5);
     }
     
 }
