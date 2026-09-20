12419b8ee778f8fa6af6067af8ca8ad41bf1ec87
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-09-20 07:54:59 -0700

    Update Exceptions file

--- oops/Exceptions.java
+++ oops/Exceptions.java
@@ -2,13 +2,23 @@ public class Exceptions {
     public static void main(String args[])
     {
         int i=18;
-        int j=0;
+        int j=18;
+        int nums[]=new int[5];
+        String str=null;
         try{
-            j=i/j; //risky statements should be in try block
+            // j=i/j; //risky statements should be in try block
+            // System.out.println(nums[6]);
+            System.out.println(str.length());
         }
-        catch(Exception e){ //error is thrown as an object and Exception here is a class
-            System.out.println("Something went wrong");
+        catch(ArrayIndexOutOfBoundsException e) //to handle array exception
+        {
+            System.out.println("Please enter nos that are within the limits");
         }
-        System.out.println("Result is "+j);
+        catch(ArithmeticException e){ //error is thrown as an object and Exception here is a class
+            System.out.println("Something went wrong ");
+        }
+        catch(NullPointerException e){ //to handle string exception
+            System.out.println("Cannot print length of a null string");
+        }
     }    
 }
