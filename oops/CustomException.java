b25fb8caa174359349a63442b768fa824229ca26
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-09-24 11:46:39 -0700

    Update Custom Exceptions file

---
+++ oops/CustomException.java
@@ -0,0 +1,34 @@
+class InvalidAgeException extends Exception{
+    public InvalidAgeException(String message)
+    {
+        super(message);
+    }
+}
+
+public class CustomException{
+    
+    public static void checkAge(int age) throws InvalidAgeException {
+        if(age < 18)
+        {
+            throw new InvalidAgeException("Age must be 18 or older");
+        }
+        System.out.println("Access granted");
+    }
+    
+    public static void main(String[] args) {
+        try{
+            checkAge(16);
+        }
+        catch(InvalidAgeException e)
+        {
+            System.out.println("Exception caught: "+e.getMessage());
+        }
+        try{
+            checkAge(21);
+        }
+        catch(InvalidAgeException e)
+        {
+            System.out.println("Exception caught: "+e.getMessage());
+        }
+    }
+}
